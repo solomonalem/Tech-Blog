3f30becb9f600fd5b3918b5160fffa770de5f845
Solomon <solomonmeresa8@gmail.com>
2020-10-25 16:00:37 -0700

    adds homepage

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-// const path = require('path');
+const path = require('path');
 const exphbs = require('express-handlebars');
-// const session = require('express-session');
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
-// const helpers = require('./utils/helpers');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3002;
 
-// const sess = {
-//   secret: 'Super secret secret',
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize
-//   })
-// };
-// app.use(session(sess));
-// const hbs = exphbs.create({helpers});
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+app.use(session(sess));
+const hbs = exphbs.create({helpers});
 
-const hbs = exphbs.create({});
+// const hbs = exphbs.create({});
 
 
 
@@ -34,7 +34,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // // css file 
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // // turn on routes
 app.use(routes);
