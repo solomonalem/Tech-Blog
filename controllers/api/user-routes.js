8e0c7a463e9b71c1a64082cedaff47fb3f7b56d0
Solomon <solomonmeresa8@gmail.com>
2020-10-24 19:28:47 -0700

    adds post model

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
-// const { User} = require('../../models');
-const  User = require('../../models/User');
+const { User} = require('../../models');
 // const = require('../../utils/auth')
 
 
